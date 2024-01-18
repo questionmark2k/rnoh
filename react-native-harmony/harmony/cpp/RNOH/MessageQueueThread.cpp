ba413ce4ceb9f3f3360ffb3869f9af6ad2815470
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2024-01-18 10:01:24 +0100

    fix: Don't schedule func in MessageQueueThread if already on JS thread
    `facebook::react::EventBeat::induce`:
    ```
       * Induces the next beat to happen as soon as possible. If the method
       * is called on the proper thread, the beat must happen synchronously.
    ```
    
    Internally, our implementation calls `react::RuntimeExecutor`, which in turn runs the task with `MessageQueueThread::runOnQueue`.
    Currently, this causes the beat to _always_ be scheduled in the future, event if induced on JS thread.
    This PR fixes this.
    
    It also brings the implementation closer to iOS's `RCTMessageThread`, which can also run tasks passed to `runOnQueue` synchronously when requested from correct thread.
    
    See merge request rnoh/react-native-harmony!744

--- react-native-harmony/harmony/cpp/RNOH/MessageQueueThread.cpp
+++ react-native-harmony/harmony/cpp/RNOH/MessageQueueThread.cpp
@@ -8,7 +8,11 @@ MessageQueueThread::MessageQueueThread(std::shared_ptr<TaskExecutor> const &task
 MessageQueueThread::~MessageQueueThread(){};
 
 void MessageQueueThread::runOnQueue(std::function<void()> &&func) {
-    taskExecutor->runTask(TaskThread::JS, std::move(func));
+    if (taskExecutor->isOnTaskThread(TaskThread::JS)) {
+        func();
+    } else {
+        taskExecutor->runTask(TaskThread::JS, std::move(func));
+    }
 }
 
 void MessageQueueThread::runOnQueueSync(std::function<void()> &&func) {
