e62e40b5471289f89f0816bb8400beb13ea67c09
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2024-01-30 10:41:22 +0100

    Revert "don't schedule func in MessageQueueThread if already on JS thread"
    This reverts commit 0cb4747be60b8f5f3a322628a99bd3adf913da71
    
    See merge request rnoh/react-native-harmony!786

--- react-native-harmony/harmony/cpp/RNOH/MessageQueueThread.cpp
+++ react-native-harmony/harmony/cpp/RNOH/MessageQueueThread.cpp
@@ -8,11 +8,7 @@ MessageQueueThread::MessageQueueThread(std::shared_ptr<TaskExecutor> const &task
 MessageQueueThread::~MessageQueueThread(){};
 
 void MessageQueueThread::runOnQueue(std::function<void()> &&func) {
-    if (taskExecutor->isOnTaskThread(TaskThread::JS)) {
-        func();
-    } else {
-        taskExecutor->runTask(TaskThread::JS, std::move(func));
-    }
+    taskExecutor->runTask(TaskThread::JS, std::move(func));
 }
 
 void MessageQueueThread::runOnQueueSync(std::function<void()> &&func) {
