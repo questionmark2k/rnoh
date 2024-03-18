d8e051bb33e8854defc4d6f5f9dfd4ecea0200a3
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2024-03-18 07:42:54 +0100

    feat: pass surface id when update'ing view
    ## Summary
    <!--
    Briefly describe the purpose of the MR, for example:
    
    This PR adds support for rounded corners for components in C-API architecture. 
    -->
    This PR implements finding Surface Id for components when setting props via `SchedulerDelegateCAPI`.
    
    ## Changes
    <!-- List main changes in bullet points that can be included in the changelog. E.g.,
    - Added rounded corner support in C-API architecture.
    -->
    
    ## Test Plan
    <!-- Steps on how the changes were tested. E.g.,
    1. Open tester app.
    2. Run "ViewTest::render square with rounded corners with different radii (left/right)". -->
    
    1. Open tester app.
    2. Run animated tests.
    
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    <!-- Link related or resolved issues. E.g.,
    Related: #123 
    Closes: #456
    -->
    Closes #901
    
    See merge request rnoh/react-native-harmony!918

--- react-native-harmony/harmony/cpp/RNOH/SchedulerDelegateCAPI.cpp
+++ react-native-harmony/harmony/cpp/RNOH/SchedulerDelegateCAPI.cpp
@@ -5,17 +5,38 @@ namespace rnoh {
 
 using namespace facebook;
 
+std::optional<react::SurfaceId> findSurfaceIdForComponentInstance(ComponentInstance::Shared componentInstance) {
+    auto parentInstance = componentInstance->getParent().lock();
+    while (parentInstance != nullptr) {
+        componentInstance = std::move(parentInstance);
+        parentInstance = componentInstance->getParent().lock();
+    }
+    auto rootComponentTag = componentInstance->getTag();
+
+    // Fabric reserves numbers equal 1 mod 10 for SurfaceIds
+    if (rootComponentTag % 10 == 1) {
+        return static_cast<react::SurfaceId>(rootComponentTag);
+    }
+
+    return std::nullopt;
+}
+
 void SchedulerDelegateCAPI::synchronouslyUpdateViewOnUIThread(
-    facebook::react::Tag tag,
+    react::Tag tag,
     folly::dynamic props,
-    facebook::react::ComponentDescriptor const &componentDescriptor) {
+    react::ComponentDescriptor const &componentDescriptor) {
         auto componentInstance = m_componentInstanceRegistry->findByTag(tag);
         if (componentInstance == nullptr) {
             return;
         }
 
+        auto surfaceId = findSurfaceIdForComponentInstance(componentInstance);
+        if (!surfaceId.has_value()) {
+            return;
+        }
+
         auto oldProps = componentInstance->getProps();
-        auto newProps = componentDescriptor.cloneProps(facebook::react::PropsParserContext{1, *m_contextContainer}, oldProps, std::move(props));
+        auto newProps = componentDescriptor.cloneProps(react::PropsParserContext{surfaceId.value(), *m_contextContainer}, oldProps, std::move(props));
 
         componentInstance->setProps(newProps);
         componentInstance->finalizeUpdates();
