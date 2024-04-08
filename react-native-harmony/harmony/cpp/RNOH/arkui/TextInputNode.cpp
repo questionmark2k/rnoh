1ffb35ab3e61ea00c621ed60681feac161807ecc
fengxuanyu <fengxuanyu@huawei.com>
2024-04-08 10:59:14 +0200

    fix: setCancelButton
    ## Summary
    <!--
    Briefly describe the purpose of the MR, for example:
    
    This PR adds support for rounded corners for components in C-API architecture. 
    -->
    Fix TextInput::setCancelButtonMode wild pointer issue.
    ## Changes
    <!-- List main changes in bullet points that can be included in the changelog. E.g.,
    - Added rounded corner support in C-API architecture.
    -->
    react-native-harmony/harmony/cpp/RNOH/arkui/TextInputNode.cpp
    ## Test Plan
    <!-- Steps on how the changes were tested. E.g.,
    1. Open tester app.
    2. Run "ViewTest::render square with rounded corners with different radii (left/right)". -->
    
    1. Open tester app.
    2. 
    
    
    ## Checklist
    - [ ] Project compiles and the tester application works.
    
    ## Issue Links
    <!-- Link related or resolved issues. E.g.,
    Related: #123 
    Closes: #456
    -->
    
    See merge request rnoh/react-native-harmony!1024

--- react-native-harmony/harmony/cpp/RNOH/arkui/TextInputNode.cpp
+++ react-native-harmony/harmony/cpp/RNOH/arkui/TextInputNode.cpp
@@ -133,16 +133,10 @@ void TextInputNode::setEnterKeyType(
 
 void TextInputNode::setCancelButtonMode(
     facebook::react::TextInputAccessoryVisibilityMode mode) {
-  ArkUI_AttributeItem item;
-  if (mode == facebook::react::TextInputAccessoryVisibilityMode::Always) {
-    ArkUI_NumberValue value[] = {
-        {.i32 = static_cast<int32_t>(ARKUI_CANCELBUTTON_STYLE_INPUT)}};
-    item = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
-  } else {
-    ArkUI_NumberValue value[] = {
-        {.i32 = static_cast<int32_t>(ARKUI_CANCELBUTTON_STYLE_INVISIBLE)}};
-    item = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
-  }
+  ArkUI_NumberValue value[] = {{.i32 = mode == facebook::react::TextInputAccessoryVisibilityMode::Always
+                                  ? static_cast<int32_t>(ARKUI_CANCELBUTTON_STYLE_INPUT)
+                                  : static_cast<int32_t>(ARKUI_CANCELBUTTON_STYLE_INVISIBLE)}};
+  ArkUI_AttributeItem item = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
   maybeThrow(NativeNodeApi::getInstance()->setAttribute(
       m_nodeHandle, NODE_TEXT_INPUT_CANCEL_BUTTON, &item));
 }
